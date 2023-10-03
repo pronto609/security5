1273cf776390267801c85d16e47eda5dadc80d9d
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-03 10:03:13 +0300

    add logout logic

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         return $this->render('security/login.html.twig', [
-            'controller_name' => 'SecurityController',
+            'error' => $authenticationUtils->getLastAuthenticationError(),
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        throw new \Exception('logout() should never be reached');
     }
 }
