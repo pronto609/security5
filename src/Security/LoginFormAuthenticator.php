7241382061e1e183fe5b8ece88f025c6187eb286
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-03 11:59:03 +0300

    remember me automatically, add possibility to logout remember token if you change password

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -56,7 +56,7 @@ class LoginFormAuthenticator extends AbstractAuthenticator
                     'authenticate',
                     $request->request->get('_csrf_token')
                 ),
-                new RememberMeBadge()
+                (new RememberMeBadge())->enable()
             ]
         );
     }
