ab3559c4fd8060f50a01ebe31d914bc492397fa8
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-05 11:51:29 +0300

    add voter

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -78,11 +78,7 @@ class QuestionController extends AbstractController
      */
     public function edit(Question $question)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
-
-        if ($question->getOwner() !== $this->getUser()) {
-            throw $this->createAccessDeniedException('You are not an owner!');
-        }
+        $this->denyAccessUnlessGranted('EDIT', $question);
 
         return $this->render('question/edit.html.twig', [
             'question' => $question,
