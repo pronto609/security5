bdd270bb963bce0975221970dc89acb6b42d4f40
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-07 08:26:26 +0300

    show totp object

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticator;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-class SecurityController extends AbstractController
+class SecurityController extends BaseController
 {
     #[Route('/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
@@ -22,5 +25,21 @@ class SecurityController extends AbstractController
     public function logout()
     {
         throw new \Exception('logout() should never be reached');
+    }
+
+    #[Route("authenticate/2fa/enable", name: "app_2fa_enable")]
+    #[IsGranted('ROLE_USER')]
+    public function enable2fa(
+        TotpAuthenticator $totpAuthenticator,
+        EntityManagerInterface $entityManager
+    ) {
+        $user = $this->getUser();
+        if (!$user->isTotpAuthenticationEnabled()) {
+            $user->setTotpSecret($totpAuthenticator->generateSecret());
+
+            $entityManager->flush();
+        }
+
+        dd($user);
     }
 }
